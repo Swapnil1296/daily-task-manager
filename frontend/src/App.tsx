ed6e018181e9da1d155e2e0000c3ffed7d5ad3e9
Swapnil <swapnil.landage@indusnet.co.in>
2025-01-10 19:26:28 +0530

    initial folder structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
-
+ import {NavBar} from "./components/Navigation/NavBar"
 const App = () => {
   return (
-    <div>
+    <>
+     <NavBar />
   
+<<<<<<< Updated upstream
   <h1 className="font-extrabold text-gray-500">Welcome to Daily Task Manager</h1></div>
+=======
+    </ >
+>>>>>>> Stashed changes
   )
 }
 export default App;
